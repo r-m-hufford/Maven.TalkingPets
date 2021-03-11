5f5d52128ed2f860cf15289a48316e4ebdf759ae
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-03-10 21:43:02 -0500

    Updated main method with prompts for pet type and pet name

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -10,16 +10,34 @@ public class MainApplication {
     }
 
     public static void main(String[] args){
-        Boolean morePets = true;
 
         Console console1 = new Console();
+        MainApplication mainApp = new MainApplication();
 
-        while(morePets){
+        while(true){
             String morePetsQuestion = console1.getStringInput("Do you have any more pets?");
-            if(morePetsQuestion=="no"){
-                morePets = false;
+            if(morePetsQuestion.equalsIgnoreCase("no")){
+                break;
             }
-            String
+            String petTypeQuestion = console1.getStringInput("Please enter the type of pet:");
+            String petNameQuestion = console1.getStringInput("Please enter the name of your pet:");
+            if(petTypeQuestion.equalsIgnoreCase("cat")){
+                Cat cat1 = new Cat(petNameQuestion);
+                mainApp.addPet(cat1);
+            }
+            else if(petTypeQuestion.equalsIgnoreCase("dog")){
+                Dog dog1 = new Dog(petNameQuestion);
+                mainApp.addPet(dog1);
+            }
+            else{
+                Fish fish1 = new Fish(petNameQuestion);
+                mainApp.addPet(fish1);
+            }
+        }
+
+        for(int i=0;i<mainApp.pets.size();i++){
+            System.out.println((i+1)+". Pet Type:"+mainApp.pets.get(i).getClass().getSimpleName()+
+                    "   Name:"+mainApp.pets.get(i).getName()+"   Sound:"+mainApp.pets.get(i).speak());
         }
 
     }
